1c938c0c6135cd3e30c5673aa1f0c0028e57089f
Matthew Lock <matthew_lock20@hotmail.com>
2021-10-04 20:01:00 +0100

    mainHeader added to main screen

--- Lubli Ghibli/Controller/ViewController.swift
+++ Lubli Ghibli/Controller/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var background: UIImageView!
+    @IBOutlet var mainHeading: UILabel!
+    @IBOutlet var categoryPicker: UIPickerView!
+    
     var dataManager = DataManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         background.alpha = 0.5
         background.contentMode = .scaleToFill
+        setupView()
 
         dataManager.getFilms { films in
             //let filteredFilms = films.filter( {$0.title.contains("Castle") } )
@@ -28,4 +32,13 @@ class ViewController: UIViewController {
 
     }
 
+    private func setupView() {
+        mainHeading.text = "LUBLI GHIBLI"
+        mainHeading.font = UIFont(name: "ITC Avant Garde Gothic", size: 40.0)
+        mainHeading.sizeToFit()
+        mainHeading.textAlignment = .center
+        mainHeading.alpha = 0.8
+    }
+    
+    
 }
