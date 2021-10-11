c5c8bd17e601ae12b56cc7139733b0e2b7738c94
Matthew Lock <matthew_lock20@hotmail.com>
2021-10-11 20:31:42 +0100

    Removed UITextfield, UISearchBar now setup and responding to user input, output in terminal

--- Lubli Ghibli/Controller/ViewController.swift
+++ Lubli Ghibli/Controller/ViewController.swift
@@ -7,72 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchResultsUpdating {
 
     @IBOutlet var background: UIImageView!
-    @IBOutlet var mainHeading: UILabel!
-    @IBOutlet var searchField: UITextField!
     
+    let searchController = UISearchController(searchResultsController: nil)
     var dataManager: DataManager?
     
     let font = "ITC Avant Garde Gothic"
-    var imageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         background.alpha = 0.5
         background.contentMode = .scaleToFill
         background.translatesAutoresizingMaskIntoConstraints = false
-        setupView()
-        searchField.delegate = self
-        searchField.backgroundColor = .lightGray
-        searchField.font = UIFont(name: font, size: 20.0)
-    }
-
-    private func setupView() {
-        mainHeading.text = "LUBLI GHIBLI"
-        mainHeading.font = UIFont(name: font, size: 40.0)
-        mainHeading.sizeToFit()
-        mainHeading.textAlignment = .center
-        mainHeading.alpha = 0.8
-    }
-
-}
-
-extension ViewController: UITextFieldDelegate {
-    
-    @IBAction func searchFieldPressed(_ sender: UITextField) {
-        searchField.endEditing(true)
-        print(searchField.text!)
+        title = "LUBLI GHIBLI"
+        searchController.searchBar.sizeToFit()
+        searchController.searchResultsUpdater = self
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchController
+        searchController.becomeFirstResponder()
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        searchField.endEditing(true)
-        print(searchField.text!)
-        return true
-    }
-    
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        if textField.text != "" {
-            return true
-        } else {
-            textField.placeholder = "Type something"
-            return false
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else {
+            return
         }
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let userText = searchField.text {
-            dataManager = DataManager(userSearchTerm: userText)
-            dataManager?.getFilms{ films in
-                //let filteredFilms = films.filter( {$0.title.contains("castle") } )
-                for film in films {
-                    if film.title.lowercased().contains(userText.lowercased()) {
-                        print(film.title)
-                    }
+        dataManager = DataManager(userSearchTerm: text)
+        dataManager?.getFilms{ films in
+            for film in films {
+                if film.title.contains(text) {
+                    print(film.title)
                 }
             }
         }
-        searchField.text = ""
+        
     }
 }
